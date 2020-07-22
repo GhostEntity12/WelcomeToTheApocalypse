3f294e7ee6eb1a1117ea9bb21a9bcd23fe2facb4
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2020-07-22 21:15:12 +1000

    Removed sample assets, added TextMeshPro, added scripts that allow for easy fading of elements

---
+++ Assets/Scripts/Callbacks.cs
@@ -0,0 +1,9 @@
+﻿/// <summary>
+/// Class for callbacks
+/// </summary>
+public static class Callbacks
+{
+    public delegate void CallbackDelegateNull();
+    public delegate void CallbackDelegateInt(int intIn);
+    public delegate void CallbackDelegateString(string stringIn);
+}
