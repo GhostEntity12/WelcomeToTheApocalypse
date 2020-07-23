6ee2803deea4010f83ff44634ad62eb8a45f07d1
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2020-07-23 18:07:56 +1000

    Moved Fade and Callbacks classes to the Ghost namespace

--- Assets/Scripts/Callbacks.cs
+++ Assets/Scripts/Callbacks.cs
@@ -1,9 +1,13 @@
 ﻿/// <summary>
 /// Class for callbacks
 /// </summary>
-public static class Callbacks
+
+namespace Ghost
 {
-    public delegate void CallbackDelegateNull();
-    public delegate void CallbackDelegateInt(int intIn);
-    public delegate void CallbackDelegateString(string stringIn);
+    public static class Callbacks
+    {
+        public delegate void CallbackDelegateNull();
+        public delegate void CallbackDelegateInt(int intIn);
+        public delegate void CallbackDelegateString(string stringIn);
+    }
 }
