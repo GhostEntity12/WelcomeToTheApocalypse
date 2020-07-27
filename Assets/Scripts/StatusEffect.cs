117388f47c7440559a6078cada166c266a909b6d
Grant <s191075@students.aie.edu.au>
2020-07-27 12:19:48 +1000

    Added StatusEffect

---
+++ Assets/Scripts/StatusEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum TriggerType
+{
+    OnTurnStart,
+    OnDealDamage,
+    OnTakeDamage
+}
+
+public abstract class StatusEffect : MonoBehaviour
+{
+    // How this status effect is triggered.
+    public TriggerType m_TriggerType;
+
+    // Icon for the status effect.
+    public Sprite m_StatusIcon;
+
+    // Description of the status effect.
+    public string m_StatusDescription;
+
+    // Check the preconditions for the status effect to take effect.
+    protected abstract void CheckPrecondition(TriggerType trigger);
+}
