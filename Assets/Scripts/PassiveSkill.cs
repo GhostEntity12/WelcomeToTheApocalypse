b029e44f9f4e1bd8d2e9f1b9055af9ed13a42cc1
Grant <s191075@students.aie.edu.au>
2020-07-28 11:24:10 +1000

    Added PassiveSkill.cs

---
+++ Assets/Scripts/PassiveSkill.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PassiveSkill : StatusEffect
+{
+    // Check the preconditions for the status effect to take effect.
+    protected abstract override bool CheckPrecondition(TriggerType trigger);
+}
