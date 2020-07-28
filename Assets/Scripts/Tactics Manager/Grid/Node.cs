8c5db2324b0ec2993224d884bcb0f43259f66b6d
Connor <connor.li.1597@outlook.com>
2020-07-28 17:08:01 +1000

    Edited Grid and Node
    Added pathfinding variables to node and Added setting nodes neighbours to Grid and some testing code

--- Assets/Scripts/Tactics Manager/Grid/Node.cs
+++ Assets/Scripts/Tactics Manager/Grid/Node.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Node
 {
+
+	public int[] m_neighbours = new int[8];
 	public int x;
 	public int y;
 	public int z;
+	public float fCost;
+	public float gCost;
 
 	public bool isWalkable;
 
@@ -17,4 +21,19 @@ public class Node
 	public GameObject unit;
 
 	public GameObject tile;
+
+
+	[Header("Breadth First Search")]
+	public bool visited = false;
+	public Node parentNode = null;
+	public int distance = 0;
+
+	public List<Node> adjacentNodes = new List<Node>();
+
+	public void Reset()
+	{
+		visited = false;
+		parentNode = null;
+		distance = 0;
+	}
 }
