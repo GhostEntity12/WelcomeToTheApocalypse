a7e235bde9c75eef2747a53a23386c814685d407
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2020-08-03 10:42:38 +1000

    Basic skills framework

---
+++ Assets/Scripts/Skills/StatusSkill.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Skills/Status Skill")]
+public class StatusSkill : DamageSkill
+{
+	public InflictableStatus m_Effect;
+
+	public override void CastSkill()
+	{
+		base.CastSkill();
+
+		// Apply status/refresh if it already exists
+	}
+}
