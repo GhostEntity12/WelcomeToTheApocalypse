729dc5f4583f318c3cfe9fac8fc3e97dc83236bb
James <james.goutos16@gmail.com>
2020-08-17 16:33:03 +1000

    Added more to blackboard

--- Assets/Scripts/AI/Blackboard.cs
+++ Assets/Scripts/AI/Blackboard.cs
@@ -11,9 +11,25 @@ public class Blackboard : MonoBehaviour
     private static Blackboard instance = null;
 
     //Here will go most of the things that the AI will need to be a functioning player.
-    Unit enemyUnit;
+    List<Unit> enemies;
+    List<Unit> playerUnits;
+    Unit enemySelectedUnit;
+    Unit playerSelectedUnit;
     Grid grid;
+    Stack<Node> path;
+    int pathCost;
+    
     //Eventually more stuff.
+
+    private State state;
+
+    private enum State
+    { 
+        PlayerTurn,
+        EndingPlayerTurn,
+        EnemyTurn
+    }
+
 
     //On Awake
     //If there is no instance of a blackboard, create one. If there already was one, destroy it.
@@ -27,6 +43,37 @@ public class Blackboard : MonoBehaviour
         else
         {
             Destroy(this);
+        }
+    }
+
+    private void Start()
+    {
+        state = State.PlayerTurn;
+    }
+
+    private void Update()
+    {
+        //If it is not the player's turn.
+        if (state != State.PlayerTurn)
+        {
+            //Calculate which enemy unit is closest to a player unit.
+            foreach (Unit enemy in enemies)
+            {
+                if (enemy.m_Allegiance == Allegiance.Enemy)
+                {
+                    enemySelectedUnit = enemy;
+                }
+
+                foreach (Unit playerUnit in playerUnits)
+                {
+                    if (playerUnit.m_Allegiance == Allegiance.Player)
+                    {
+                        playerSelectedUnit = playerUnit;
+                    }
+
+                    grid.FindPath(enemySelectedUnit.transform.position, playerSelectedUnit.transform.position, ref path, out pathCost);
+                }
+            }
         }
     }
 
