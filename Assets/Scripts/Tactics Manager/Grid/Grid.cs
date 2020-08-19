1f435b671f9316006bf46a5592254b3704eb5b60
Connor <connor.li.1597@outlook.com>
2020-08-19 11:02:32 +1000

    Edited Grid

--- Assets/Scripts/Tactics Manager/Grid/Grid.cs
+++ Assets/Scripts/Tactics Manager/Grid/Grid.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -23,8 +23,6 @@ public class Grid : MonoBehaviour
 	int maxX, maxZ;
 	int posX, posZ;
 
-	List<Vector3> nodeViz = new List<Vector3>();
-
 	[Tooltip("Used to show the node position with something like a really small plane or something")]
 	public GameObject node;
 	[Tooltip("Used to store the objects for the nodes")]
@@ -40,11 +38,6 @@ public class Grid : MonoBehaviour
 	{
 		m_Instance = this;
 		ReadLevel();
-	}
-
-	void Start()
-	{
-		
 	}
 
 	void ReadLevel()
@@ -159,10 +152,6 @@ public class Grid : MonoBehaviour
 					n.m_tile = Instantiate(node, new Vector3(n.worldPosition.x, n.worldPosition.y + 0.01f, n.worldPosition.z), Quaternion.identity, nodeArray.transform);
 					n.m_NodeHighlight = n.m_tile.GetComponent<NodeHighlight>();
 					n.m_NodeHighlight.ChangeHighlight(TileState.None);
-				}
-				if (n.obstacle != null)
-				{
-					nodeViz.Add(n.worldPosition);
 				}
 				m_grid[x, z] = n;
 
@@ -306,6 +295,7 @@ public class Grid : MonoBehaviour
 	{
 		Node n = GetNode(unit.transform.position);
 		n.unit = unit.GetComponent<Unit>();
+		n.isWalkable = false;
 	}
 
 	public Unit GetUnit(Vector3 mousePos)
@@ -316,6 +306,7 @@ public class Grid : MonoBehaviour
 	public void RemoveUnit(Node unitNode)
 	{
 		unitNode.unit = null;
+		unitNode.isWalkable = true;
 	}
 
 	public void ClearNode()
@@ -323,15 +314,6 @@ public class Grid : MonoBehaviour
 		foreach (Node node in nodething)
 		{
 			node.m_NodeHighlight.ChangeHighlight(TileState.None);
-		}
-	}
-
-	private void OnDrawGizmos()
-	{
-		Gizmos.color = Color.black;
-		for(int i = 0; i < nodeViz.Count; ++i)
-		{
-			Gizmos.DrawWireCube(nodeViz[i], extends);
 		}
 	}
 
@@ -554,28 +536,4 @@ public class Grid : MonoBehaviour
 			return ((19 * dx) + 10 * (dz - dx));
 		}
 	}
-
-	void Update()
-	{
-		//if (!searched)
-		//{
-		//	foreach (GameObject go in unit)
-		//	{
-		//		GetArea(4, go);
-		//	}
-		//	searched = true;
-		//}
-	}
-
-	//Will be Deleted
-	[ContextMenu("Test")]
-	void Test()
-	{
-		foreach (GameObject go in unit)
-		{
-			print(go.name + " Node Pos: " + GetNode(go.transform.position).worldPosition);
-			print(go.name + " Pos: " + go.transform.position);
-		}
-	}
-	/*Multple Floor stuff will be deleted at a later date*/
 }
