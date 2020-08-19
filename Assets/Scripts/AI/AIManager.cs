74bd80613e1fa98d636b85283c443360900ee910
James <james.goutos16@gmail.com>
2020-08-19 10:28:19 +1000

    Pushed base AIManager script

---
+++ Assets/Scripts/AI/AIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIManager : MonoBehaviour
+{
+    Unit enemyAI;
+
+    private void Update()
+    {
+        if (GetCurrentTurn() == Allegiance.Enemy)
+        {
+            GetClosestPlayerUnit();
+        }
+    }
+
+    //Gets whose turn it is.
+    public Allegiance GetCurrentTurn()
+    {
+        return GameManager.m_Instance.GetCurrentTurn();
+    }
+
+    //Finds the closest player unit.
+    public void GetClosestPlayerUnit()
+    {
+        foreach (Unit units in GameManager.m_Instance.m_PlayerUnits)
+        {
+            
+        }
+    }
+
+    //Calculates a path to the closest unit.
+    public void CalculatePath()
+    {
+        
+    }
+
+    //Attacks the unit.
+    public void Attack()
+    {
+        
+    }
+}
