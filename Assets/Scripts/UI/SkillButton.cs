9f12c4151df4dc2027162ecada30c0727269d93e
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2020-10-05 14:25:06 +1100

    Quick script that will update the skills stats automatically

--- Assets/Scripts/UI/SkillButton.cs
+++ Assets/Scripts/UI/SkillButton.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,7 +7,11 @@ public class SkillButton : MonoBehaviour
 {
 	public BaseSkill m_Skill;
 
-	public Image m_Image;
+	public Image m_BgImage;
+
+	public Image m_LightImage;
+
+	public Image m_DarkImage;
 
 	public TextMeshProUGUI m_Description;
 
@@ -22,7 +27,31 @@ public class SkillButton : MonoBehaviour
 	public void UpdateTooltip()
 	{
 		print(m_Skill);
-		m_Description.text = m_Skill.m_Description;
+		string baseDescription = m_Skill.m_Description;
+		if (m_Skill is DamageSkill)
+		{
+			baseDescription = baseDescription.Replace("{damage}", (m_Skill as DamageSkill).m_DamageAmount.ToString());
+		}
+		if (m_Skill is HealSkill)
+		{
+			baseDescription = baseDescription.Replace("{heal}", (m_Skill as HealSkill).m_HealAmount.ToString());
+		}
+		if (m_Skill is StatusSkill)
+		{
+			try
+			{
+				baseDescription = baseDescription.Replace("{duration}", (m_Skill as StatusSkill).m_Effect.m_StartingDuration.ToString());
+			}
+			catch (NullReferenceException)
+			{
+				Debug.LogError("Skill is lacking a required status!");
+			}
+		}
+		baseDescription = baseDescription.Replace("{distance}", m_Skill.m_CastableDistance.ToString());
+		baseDescription = baseDescription.Replace("{range}", m_Skill.m_AffectedRange.ToString());
+		baseDescription = baseDescription.Replace("{cooldown}", m_Skill.m_CooldownLength.ToString());
+		print(baseDescription);
+		m_Description.text = baseDescription;
 	}
 
 	public void DisplayTooltip(bool show)
