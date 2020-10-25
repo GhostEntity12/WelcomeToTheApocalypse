ff22fa7fab0cd1811e110316911c9f4e888b4ee9
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2020-10-26 09:51:45 +1100

    Assorted UI

--- Assets/Scripts/RandAnimStart.cs
+++ Assets/Scripts/RandAnimStart.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class RandAnimStart : MonoBehaviour
 {
+    public bool m_RandomiseSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Animator>().SetFloat("Speed", Random.Range(0.7f, 1.5f));
-        GetComponent<Animator>().Play("Paper1", 0, Random.Range(0, GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length));
+        if (m_RandomiseSpeed)
+        {
+            GetComponent<Animator>().SetFloat("Speed", Random.Range(0.7f, 1.5f));
+        }
+        GetComponent<Animator>().Play(0, 0, Random.Range(0, GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length));
     }
 }
