90dcb1bd3f82befe7f387a0813719af64ae22d2b
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2020-11-04 15:37:40 +1100

    Ai waiting on status skills

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -456,6 +456,12 @@ public class Unit : MonoBehaviour
 		m_CurrentActionPoints = m_StartingActionPoints;
 	}
 
+	void AddStatusEffectFromSkill(InflictableStatus effect)
+	{
+		AIManager.m_Instance.m_AwaitingUnits.Remove(this);
+		AddStatusEffect(effect);
+	}
+
 	/// <summary>
 	/// Add a status effect to the unit.
 	/// </summary>
