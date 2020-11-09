056783076d9013d69e261bc89797be49b7a19eb2
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2020-11-10 01:22:03 +1100

    Set up zooms on main menu, updated posters

--- Assets/Scripts/MainMenuFunctions.cs
+++ Assets/Scripts/MainMenuFunctions.cs
@@ -1,4 +1,6 @@
-﻿using TMPro;
+﻿using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -14,12 +16,39 @@ public class MainMenuFunctions : MonoBehaviour
 	/// </summary>
 	public CanvasGroup m_BlackScreen = null;
 
+	public CanvasGroup m_Prompt = null;
+
+	public List<Renderer> m_ArtistPosters = new List<Renderer>();
+	public List<Renderer> m_DesignerPosters = new List<Renderer>();
+	public List<Renderer> m_ProgrammerPosters = new List<Renderer>();
+
+	List<Renderer> m_AllPosters = new List<Renderer>();
+
+	public Collider m_ArtistCollider = null;
+	public Collider m_DesignerCollider = null;
+	public Collider m_ProgrammingCollider = null;
+
 	/// <summary>
 	/// On startup.
 	/// </summary>
 	void Awake()
 	{
 		m_Anim = Camera.main.GetComponent<Animator>();
+
+		m_AllPosters = m_ArtistPosters.Concat(m_DesignerPosters).Concat(m_ProgrammerPosters).ToList();
+
+		foreach (Renderer renderer in m_AllPosters)
+		{
+			renderer.material = new Material(renderer.material);
+		}
+	}
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(1))
+		{
+			FromSubsection();
+		}
 	}
 
 	/// <summary>
@@ -62,5 +91,48 @@ public class MainMenuFunctions : MonoBehaviour
 	public void ToggleGlow(TextMeshPro text)
 	{
 		text.fontMaterial.SetFloat("_GlowPower", text.fontMaterial.GetFloat("_GlowPower") == 0 ? 1 : 0);
+	}
+
+	public void ToggleGlow(string disciplinePosters)
+	{
+		List<Renderer> postersToUpdate = new List<Renderer>();
+		switch (disciplinePosters.ToLower())
+		{
+			case "art":
+				postersToUpdate = m_ArtistPosters;
+				break;
+			case "design":
+				postersToUpdate = m_DesignerPosters;
+				break;
+			case "programming":
+				postersToUpdate = m_ProgrammerPosters;
+				break;
+			default:
+				break;
+		}
+		foreach (Renderer renderer in postersToUpdate)
+		{
+			renderer.material.SetFloat("_DoOutline", renderer.material.GetFloat("_DoOutline") == 1 ? 0 : 1);
+		}
+	}
+
+	public void ToSubsection(string boolName)
+	{
+		m_Anim.SetBool(boolName, true);
+		m_ArtistCollider.enabled = false;
+		m_DesignerCollider.enabled = false;
+		m_ProgrammingCollider.enabled = false;
+		LeanTween.alphaCanvas(m_Prompt, 1, 0.5f);
+	}
+
+	public void FromSubsection()
+	{
+		m_ArtistCollider.enabled = true;
+		m_DesignerCollider.enabled = true;
+		m_ProgrammingCollider.enabled = true;
+		m_Anim.SetBool("isArtists", false);
+		m_Anim.SetBool("isDesigners", false);
+		m_Anim.SetBool("isProgrammers", false);
+		LeanTween.alphaCanvas(m_Prompt, 0, 0.5f);
 	}
 }
