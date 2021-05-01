9afe85c3c4e325adf6f9c9835087e176b8f5fd30
Grant <gred.roberts@gmail.com>
2021-05-01 18:52:29 +1000

    Group formations for free movement implemented
    Stuff like rotating the formation with the group so they face forwards isn't in, they'll always face the same direction no matter where they're moving from

---
+++ Assets/Scripts/Formation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Formation : MonoBehaviour
+{
+	[SerializeField]
+	private Transform m_DeathPosition = null;
+
+	[SerializeField]
+	private Transform m_PestilencePosition = null;
+
+	[SerializeField]
+	private Transform m_FaminePosition = null;
+
+	[SerializeField]
+	private Transform m_WarPosition = null;
+
+	public Transform GetDeathPosition()
+	{
+		return m_DeathPosition;
+	}
+	public Transform GetPestilencePosition()
+	{
+		return m_PestilencePosition;
+	}
+	public Transform GetFaminePosition()
+	{
+		return m_FaminePosition;
+	}
+	public Transform GetWarPosition()
+	{
+		return m_WarPosition;
+	}
+}
